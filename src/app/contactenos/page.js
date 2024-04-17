d6b0af8a06bca81c2f04abd4d03d85123a45e8cf
eduardojzv <ezamorava@est.utn.ac.cr>
2024-04-16 23:08:46 -0600

    correciones de diseño

--- src/app/contactenos/page.js
+++ src/app/contactenos/page.js
@@ -10,21 +10,21 @@ export default function Contact() {
         <ContacUsForms />
       </div>
       {/*col 2*/}
-        <div>
-          <ul>
-            <li><p> Hable con nosotros por Teléfono, Corre Electrónico o Chat,
-               No recibimos llamadas mediante Whatsapp</p></li><br></br>
-            <li><p> <strong>Horarios</strong></p>
-            </li>
-            <li><p> Viernes</p></li><br></br>
-          </ul>
-          <ul>
-            <li><p><strong> Teléfono:  </strong>6175-8264 / 6077-9967</p></li><br></br>
-            <li><p> <strong> Whatsapp:  </strong>6175-8264 / 6077-9967</p> </li><br></br>
-            <li><span> <strong>email:</strong> <a className="text-blue-600" href="mailto:greenzonetours506@gmail.com">AllBlueTours@gmail.com</a></span></li>
-          </ul>
-        </div>
+      <div>
+        <ul>
+          <li><p> Hable con nosotros por Teléfono, Corre Electrónico o Chat,
+            No recibimos llamadas mediante Whatsapp</p></li><br></br>
+          <li><p> <strong>Horarios</strong></p>
+          </li>
+          <li><p> Viernes</p></li><br></br>
+        </ul>
+        <ul>
+          <li><p><strong> Teléfono:  </strong>6175-8264 / 6077-9967</p></li><br></br>
+          <li><p> <strong> Whatsapp:  </strong>6175-8264 / 6077-9967</p> </li><br></br>
+          <li><span> <strong>email:</strong> <a className="text-blue-600" href="mailto:greenzonetours506@gmail.com">AllBlueTours@gmail.com</a></span></li>
+        </ul>
       </div>
-   
+    </div>
+
   )
 }
